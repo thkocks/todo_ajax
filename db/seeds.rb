41c788715dab8ecc56030a3e204e2678e918548f
thkocks <timhkocks@gmail.com>
2017-06-02 10:16:02 +0200

    Set up project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.delete_all
+
+Todo.create!([
+  { title: "Work on project", completed: true },
+  { title: "Learn AJAX", completed: false },
+  { title: "Drink beer", completed: false },
+  ])
